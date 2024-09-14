d5f67ee8eff708da688faaae28ba35f58f55e4d1
Crixus1988 <kbonch@gmail.com>
2024-09-14 16:36:02 -0500

    fix league index

--- src/AppReducer.jsx
+++ src/AppReducer.jsx
@@ -3,7 +3,7 @@ const appReducer = (state, action) => {
         case 'set_is_mobile':
             return { ...state, isMobile: action.payload };
         case 'set_league': 
-            return { ...state, leagueId: action.payload };
+            return { ...state, leagueIndex: action.payload };
         default:
             return state;
     }
